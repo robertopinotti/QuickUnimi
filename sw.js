a94c79641829d66521a359f9d20a3fc1051f5d94
Roberto Pinotti <rpinotti92@gmail.com>
2021-01-27 12:23:05 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,29 @@
+// INSTALL
+
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('quickunimi-cache').then(function(cache) {
     console.log('[ServiceWorker] Pre-caching offline page');
     return cache.addAll([
-       'index.html'
+        'index.html',
+        'manifest.json',
+        'sw.js',
+        'service-worker.js'
     ]);
    })
  );
 });
 
+// FETCH
+
 self.addEventListener('fetch', function(e) {
   
+  console.log('[ServiceWorker] fetch');
   console.log(e.request.url);
   
   e.respondWith(
 
-    caches.open('quickunimi-cache').then(cache => {
+    caches.open('quickunimi-cache').then(function(cache) {
 
       return cache.match(e.request).then(
         cacheResponse => cacheResponse || fetch(evt.request).then(
