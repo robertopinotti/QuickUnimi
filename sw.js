0ce38a250311b1b556ff8d75fe8f895178011df3
Roberto Pinotti <rpinotti92@gmail.com>
2021-01-27 12:11:44 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,33 @@
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('video-store').then(function(cache) {
-     return cache.addAll([
+   caches.open('quickunimi-cache').then(function(cache) {
+    console.log('[ServiceWorker] Pre-caching offline page');
+    return cache.addAll([
        'index.html'
-     ]);
+    ]);
    })
  );
 });
 
 self.addEventListener('fetch', function(e) {
+  
   console.log(e.request.url);
+  
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+
+    cache.open('quickunimi-cache').then(cache => {
+
+      return cache.match(e.request).then(
+        cacheResponse => cacheResponse || fetch(evt.request).then(
+          networkResponse => {
+            cache.put(e.request, networkResponse.clone());
+            return networkResponse;
+          } // networkResponse
+        ) // cacheResponse
+      ) // return cache
+
+    }) // cache.open
+
+  ) // e.respondWith
+
+}); // self
